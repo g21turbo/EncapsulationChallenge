9285f273fe52c71bda994ebfab5c789ac2b47a25
g21turbo <dg21stew@gmail.com>
2024-03-11 22:13:56 -0700

    Initial commit

---
+++ src/Printer.java
@@ -0,0 +1,16 @@
+public class Printer {
+
+    int tonerLevel;
+    int pagesPrinted;
+    boolean duplex;
+
+    public int addToner(int tonerAmount) {
+
+    }
+
+    public int printPages() {
+
+    }
+
+
+}
