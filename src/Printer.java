f96076a7e22ebeb1c68a4d524a72f7cffd4496d8
g21turbo <dg21stew@gmail.com>
2024-03-11 22:29:04 -0700

    added printPages and addToner methods

--- src/Printer.java
+++ src/Printer.java
@@ -1,16 +1,27 @@
 public class Printer {
 
-    int tonerLevel;
+    int tonerLevel = 100;
     int pagesPrinted;
-    boolean duplex;
+    boolean duplex = true;
 
     public int addToner(int tonerAmount) {
+        tonerLevel = tonerLevel + tonerAmount;
 
+        if (tonerLevel > 100 || tonerLevel < 0) {
+            return -1;
+        } else {
+            return tonerLevel;
+        }
     }
 
-    public int printPages() {
+    public int printPages(int pages) {
 
+        if (!duplex) {
+            return pages;
+        } else {
+            pages = pages / 2;
+        }
+        return pages;
     }
-
 
 }
