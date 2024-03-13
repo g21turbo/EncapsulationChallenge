a651aaaf05de8be07bc8e96fc150141e2513782f
g21turbo <dg21stew@gmail.com>
2024-03-12 20:14:30 -0700

    created printer object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Printer printer = new Printer(75, false);
+        System.out.println("Initial printed page count = " + printer.getPagesPrinted());
+
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n",
+                    pagesPrinted, printer.getPagesPrinted());
+
+
+
     }
 }
